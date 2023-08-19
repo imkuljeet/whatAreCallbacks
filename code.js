c3290570b1e5bff3fefd48a3e1fed478eac574ae
imkuljeet <iamkkashyap@gmail.com>
2023-08-19 23:02:24 +0530

    accordinToHintVideo

--- code.js
+++ code.js
@@ -1,11 +1,11 @@
-const posts = [{title: 'Post One', body: 'This is post one'},{title: 'Post Two', body: 'This is post two'}];
+const posts = [{title: 'Post One', body: 'This is post one',createdAt: new Date().getTime()},{title: 'Post Two', body: 'This is post two',createdAt: new Date().getTime()}];
 
 function getPosts() {
   setTimeout(() => {
     let output ='';
 
     posts.forEach((post,index) => {
-      output = output + `<li>${post.title}`;
+      output = output + `<li>${post.title} - last updated ${newDate().getTime - post.createdAt}`;
     });
     document.body.innerHTML = output;
   },1000);
@@ -13,10 +13,11 @@ function getPosts() {
 
 function createPost(post , callback) {
   setTimeout(() => {
-    posts.push(post);
+    posts.push({...post,createdAt:newDate().getTime()});
     callback();
   },2000);
 }
 
 createPost({title: 'Post Three', body: 'This is post three'},getPosts);
+create4thPost({title: 'Post Four', body: 'This is post four'},getPosts);
 
