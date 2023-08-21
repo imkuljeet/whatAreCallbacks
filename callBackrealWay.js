55fb7a035d07aa0be78f633226c3f3e5eece3bb2
imkuljeet <139719986+imkuljeet@users.noreply.github.com>
2023-08-21 22:29:10 +0530

    complete callbackreal

--- callBackrealWay.js
+++ callBackrealWay.js
@@ -3,31 +3,35 @@ const posts = [
   { title: 'Post Two', body: 'This is post two', createdAt: new Date().getTime() }
 ];
 
-function getPosts(){
-  setTimeout(()=>{
+let intervalId; // Declare intervalId
+
+function getPosts() {
+  clearInterval(intervalId);
+
+  intervalId = setInterval(() => { // Corrected typo
     let str = "";
-    for(let i=0;i<posts.length;i++){
-      str = str +`<li>${posts[i].title} - last updated ${(new Date().getTime() - posts[i].createdAt)/1000} seconds</li>`
+    for (let i = 0; i < posts.length; i++) {
+      str = str + `<li>${posts[i].title} - last updated ${(new Date().getTime() - posts[i].createdAt) / 1000} seconds</li>`;
     }
     document.body.innerHTML = str;
-  },1000);
+  }, 1000);
 }
 
 getPosts();
 
-function createPost(post,callback){
-  setTimeout(()=>{
-    posts.push({...post,createdAt: new Date().getTime()})
-  callback();
-  },2000);
+function createPost(post, callback) {
+  setTimeout(() => {
+    posts.push({ ...post, createdAt: new Date().getTime() });
+    callback();
+  }, 2000);
 }
 
-function create4thPost(post,callback){
-  setTimeout(()=>{
-    posts.push({...post,createdAt: new Date().getTime()})
-  callback();
-  },6000);
+function create4thPost(post, callback) {
+  setTimeout(() => {
+    posts.push({ ...post, createdAt: new Date().getTime() });
+    callback();
+  }, 6000);
 }
 
-createPost({ title: 'Post Three', body: 'This is post three', createdAt: new Date().getTime() },getPosts);
-create4thPost({ title: 'Post Four', body: 'This is post four', createdAt: new Date().getTime() },getPosts);
+createPost({ title: 'Post Three', body: 'This is post three', createdAt: new Date().getTime() }, getPosts);
+create4thPost({ title: 'Post Four', body: 'This is post four', createdAt: new Date().getTime() }, getPosts);
